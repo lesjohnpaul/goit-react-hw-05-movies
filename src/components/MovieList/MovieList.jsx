89dfac148377c3b38b4fdb6efb7d7b03093f1f58
lesjohnpaul <55284022+lesjohnpaul@users.noreply.github.com>
2024-05-02 15:31:23 +0800

    Wrap both image and title in the Link component

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -8,21 +8,21 @@ const MovieList = ({ movies }) => {
       <ul>
         {movies.map(({ title, id, poster_path }) => (
           <li key={id} className={styles['movie-list-item']}>
-            <img
-              src={
-                poster_path
-                  ? `https://image.tmdb.org/t/p/w185${poster_path}`
-                  : 'https://live.staticflickr.com/65535/51349451747_f6d7898f2c_n.jpg'
-              }
-              alt={title}
-              className={styles.moviePoster}
-            />
             <Link
               to={`/movies/${id}`}
-              state={location}
+              state={{ background: location }}
               className={styles['movie-link']}
             >
-              {title}
+              <img
+                src={
+                  poster_path
+                    ? `https://image.tmdb.org/t/p/w185${poster_path}`
+                    : 'https://live.staticflickr.com/65535/51349451747_f6d7898f2c_n.jpg'
+                }
+                alt={title}
+                className={styles.moviePoster}
+              />
+              <div>{title}</div>
             </Link>
           </li>
         ))}
